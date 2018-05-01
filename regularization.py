7b0a6fe51da31ce9d852ed01e2d8764b988baa8c
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2018-05-01 19:49:00 +0530

    Add files via upload

---
+++ regularization.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  1 19:36:12 2018
+
+@author: Abhishek
+
+Selecting meaningful features
+-----------------------------
+L1 and L2 regularization as penalties against model complexity
+
+A geometric interpretation of L2 regularization
+-----------------------------------------------
+Sparse solutions with L1-regularization
+For regularized models in scikit-learn that support L1 regularization, 
+we can simply set the `penalty` parameter to `'l1'` to obtain a sparse solution:
+
+"""
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+
+df_wine = pd.read_csv('https://archive.ics.uci.edu/'
+                      'ml/machine-learning-databases/wine/wine.data',
+                      header=None)
+
+
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
+                   'Color intensity', 'Hue', 'OD280/OD315 of diluted wines',
+                   'Proline']
+
+print('Class labels', np.unique(df_wine['Class label']))
+df_wine.head()
+
+
+
+
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+
+X_train, X_test, y_train, y_test =    train_test_split(X, y, 
+                     test_size=0.3, 
+                     random_state=0, 
+                     stratify=y)
+
+mms = MinMaxScaler()
+X_train_norm = mms.fit_transform(X_train)
+X_test_norm = mms.transform(X_test)
+
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+
+
+LogisticRegression(penalty='l1')
+
+
+# Applied to the standardized Wine data ...
+
+
+
+
+lr = LogisticRegression(penalty='l1', C=1.0)
+lr.fit(X_train_std, y_train)
+print('Training accuracy:', lr.score(X_train_std, y_train))
+print('Test accuracy:', lr.score(X_test_std, y_test))
+
+
+
+
+lr.intercept_
+
+
+
+
+np.set_printoptions(8)
+
+
+
+
+lr.coef_[lr.coef_!=0].shape
+
+
+
+
+lr.coef_
+
+
+
+
+
+fig = plt.figure()
+ax = plt.subplot(111)
+    
+colors = ['blue', 'green', 'red', 'cyan', 
+          'magenta', 'yellow', 'black', 
+          'pink', 'lightgreen', 'lightblue', 
+          'gray', 'indigo', 'orange']
+
+weights, params = [], []
+for c in np.arange(-4., 6.):
+    lr = LogisticRegression(penalty='l1', C=10.**c, random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column + 1],
+             color=color)
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center', 
+          bbox_to_anchor=(1.38, 1.03),
+          ncol=1, fancybox=True)
+plt.show()
